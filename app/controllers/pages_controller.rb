b699370a8e78a90d72ae4bb1eac8477d99360f66
josephrmoore <josephrmoore@gmail.com>
2011-02-28 19:51:57 -0500

    images

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,18 +1,22 @@
 class PagesController < ApplicationController
   def home
     @title = "Home"
+    @logo = "logo_large.png"
   end
 
   def contact
     @title = "Contact"
+    @logo = "logo_medium.png"
   end
   
   def about
     @title = "About"
+    @logo = "logo_small.png"
   end
   
   def help
     @title = "Help"
+    @logo = "logo_awesome.png"
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 module ApplicationHelper
+  
+  def logo
+    if @logo.nil?
+      image_tag("default.png", :alt => "Sample App", :class => "round")
+    else
+      image_tag(@logo, :alt => "Sample App", :class => "round")
+    end
+  end
+  
   # Return a title on a per-page basis.
   def title
     base_title = "Ruby on Rails Tutorial Sample App"
