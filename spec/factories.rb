d00cb90543e4971dacc1a16852f4c7ff19844fea
josephrmoore <josephrmoore@gmail.com>
2011-03-28 11:20:48 -0400

    up to name and gravatar 7.3.2

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                   "Joe Moore"
+  user.email                  "joe@moore.com"
+  user.password               "foobar"
+  user.password_confirmation  "foobar"
+end
